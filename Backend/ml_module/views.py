dba182ab239ef609e6e77843d5cccadf4ab1d0f9
Incognitosin01 <ssmahajan@mitaoe.ac.in>
2022-03-13 18:06:38 +0530

    requirements added

--- Backend/ml_module/views.py
+++ Backend/ml_module/views.py
@@ -16,17 +16,15 @@ def crop_recommend(Nitrogen,Phosphorous,Potassium,temp,humidity,ph_level,rainfal
     return XB.predict(np.array([[Nitrogen,Phosphorous,Potassium,temp,humidity,ph_level,rainfall]]))
 
 def crop_recommendations(request):
-    # Nitrogen = request.POST['nitrogen']
-    # Phosphorous = request.POST['Phosphorous']
-    # Potassium = request.POST['Potassium']
-    # temp = request.POST['temp']
-    # humidity = request.POST['humidity']
-    # ph_level = request.POST['ph']
-    # rainfall = request.POST['rainfall']
-    # ans = crop_recommendations(Nitrogen,Phosphorous,Potassium,temp,humidity,ph_level,rainfall)
-    ans = crop_recommend(83, 45, 60, 28, 70.3, 7.0, 150.9)
+    Nitrogen = request.POST['nitrogen']
+    Phosphorous = request.POST['Phosphorous']
+    Potassium = request.POST['Potassium']
+    temp = request.POST['temp']
+    humidity = request.POST['humidity']
+    ph_level = request.POST['ph']
+    rainfall = request.POST['rainfall']
+    ans = crop_recommendations(Nitrogen,Phosphorous,Potassium,temp,humidity,ph_level,rainfall)
+    # ans = crop_recommend(83, 45, 60, 28, 70.3, 7.0, 150.9)
     return JsonResponse({"predicted_value":str(ans)})
 
 
-
-    
